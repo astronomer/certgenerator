79e2dc90fc7c111b2d7b0fce3566767ff3c4228d
pgvishnuram <vishnu@astronomer.io>
2021-12-24 15:57:16 +0530

    added kind functional test

---
+++ tests/functionaltests/test_certgenerator.py
@@ -0,0 +1,52 @@
+from pytest_kind import KindCluster
+from pytest import fixture
+from pykube import Pod, Secret
+import operator
+import time
+
+
+@fixture(scope="session")
+def kind_cluster():
+    cluster = KindCluster("certgenerator")
+    cluster.create()
+    yield cluster
+    cluster.delete()
+
+
+def test_k8s_cluster(kind_cluster):
+    print(kind_cluster.kubeconfig_path)
+    assert kind_cluster.name == "certgenerator"
+
+
+def test_k8s_api_version(kind_cluster):
+    assert kind_cluster.api.version == ("1", "20")
+
+
+def test_k8s_pod(kind_cluster):
+    kind_cluster.load_docker_image("/tmp/workspace/certgenerator.tar")
+    kind_cluster.kubectl("create", "ns", "astronomer")
+    kind_cluster.kubectl("apply", "-f", "rbac.yaml")
+    kind_cluster.kubectl("apply", "-f", "generate_ssl.yaml")
+    time.sleep(60)
+    pods = Pod.objects(kind_cluster.api).filter(selector="component=certgenerator")
+    for pod in pods:
+        status = Pod.objects(kind_cluster.api).filter(namespace="default").get(name=pod)
+        assert "Completed" in pod.obj["status"]["phase"]
+
+
+def test_k8s_tls_secret(kind_cluster):
+    secrets = Secret.objects(kind_cluster.api).filter(
+        namespace="astronomer",
+        selector="component=certgenerator-pgbouncer-client-certificates",
+    )
+    for secret in secrets:
+        secret_name = (
+            Secret.objects(kind_cluster.api)
+            .filter(namespace="astronomer")
+            .get(name=secret)
+        )
+        print(secret.obj["metadata"]["name"])
+        assert (
+            "certgenerator-pgbouncer-client-certificates"
+            == secret.obj["metadata"]["name"]
+        )
